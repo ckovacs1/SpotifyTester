002a796aaf85b7130d179687fd6d630c9591c135
Christian Kovacs <66984388+ckovacs1@users.noreply.github.com>
2021-06-17 13:50:06 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,13 +1,17 @@
 import requests
 
-#insert user id and playlist id for this to actually work 
+#url to create playlist insert user id and playlist id for this to actually work 
 createPlaylistURL = 'https://api.spotify.com/v1/users/{user_id}/playlists'
+
+#spotify url for adding a song to playlist insert the playlist in the url
 addSongURL        = 'https://api.spotify.com/v1/playlists/{playlist_id}/tracks'
 
 #insert auth token 
 accessToken = ''
 
 def createPlaylist(name, public):
+    
+    #calls the url with the access token and create a playlist with a name and public setting
     response = requests.post(
         createPlaylistURL,
         headers={
@@ -19,6 +23,7 @@ def createPlaylist(name, public):
         }
     )
 
+    #returns the json response
     json_resp = response.json()
     return json_resp
 
@@ -26,8 +31,11 @@ def createPlaylist(name, public):
 
 
 def main():
+    
+    #creates a playlist with the name: playlist and a private setting
     playlist = createPlaylist("playlist", False)
 
+    #prints confimation
     print(f"Playlist: {playlist}")
 
 if __name__ == '__main__':
